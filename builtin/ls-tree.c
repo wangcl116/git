c9534cc0c5c76ac6993ea7962952a4603572b9b3
Teng Long <dyroneteng@gmail.com>
2022-02-08 20:14:31 +0800

    ls-tree: simplify nesting if/else logic in "show_tree()"
    This commit use "object_type()" to get the type, then remove
    some of the nested if to let the codes here become more cleaner.
    
    Signed-off-by: Teng Long <dyronetengb@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/ls-tree.c
+++ builtin/ls-tree.c
@@ -66,19 +66,13 @@ static int show_tree(const struct object_id *oid, struct strbuf *base,
 {
 	int recurse = 0;
 	size_t baselen;
-	enum object_type type = OBJ_BLOB;
+	enum object_type type = object_type(mode);
 
-	if (S_ISGITLINK(mode)) {
-		type = OBJ_COMMIT;
-	} else if (S_ISDIR(mode)) {
-		if (show_recursive(base->buf, base->len, pathname)) {
-			recurse = READ_TREE_RECURSIVE;
-			if (!(ls_options & LS_SHOW_TREES))
-				return recurse;
-		}
-		type = OBJ_TREE;
-	}
-	else if (ls_options & LS_TREE_ONLY)
+	if (type == OBJ_TREE && show_recursive(base->buf, base->len, pathname))
+		recurse = READ_TREE_RECURSIVE;
+	if (type == OBJ_TREE && recurse && !(ls_options & LS_SHOW_TREES))
+		return recurse;
+	if (type == OBJ_BLOB && (ls_options & LS_TREE_ONLY))
 		return 0;
 
 	if (!(ls_options & LS_NAME_ONLY)) {
